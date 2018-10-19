23d74c3c5708468c961b2b9d985b42b95143b1b4
marcogagne2 <marcogagne2@outlook.fr>
2018-10-19 09:37:31 -0400

    exercise 1 terminer

--- Exercises/Exercise1/JS/traitement.js
+++ Exercises/Exercise1/JS/traitement.js
@@ -3,7 +3,7 @@ function btnCalculer_onclick()
      var PoidsLivres,kg;
 
      PoidsLivres=parseFloat(document.getElementById("txtPoids").value);
-     kg=(PoidsLivres/2.2)
+     kg=(PoidsLivres/2.2);
      console.log("Votre poids en kg est "+kg);
 
 }
