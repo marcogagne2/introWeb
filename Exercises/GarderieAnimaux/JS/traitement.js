8ce3f2edac08756cb99ce0f8b454d7d16887e242
marcogagne2 <marcogagne2@outlook.fr>
2018-10-24 12:59:42 -0400

    exercise animalerie Terminer

--- Exercises/GarderieAnimaux/JS/traitement.js
+++ Exercises/GarderieAnimaux/JS/traitement.js
@@ -1,28 +1,35 @@
 function btnCalculer_onclick()
 { 
- 	var ChoixVet,NbrJours,Toilettage,race,cout;
+ 	var ChoixVet,NbrJours,Toilettage,Toilettagemot,race,cout,Total;
 
  	ChoixVet=document.getElementById("lstVeterinaire").value;
  	NbrJours=parseInt(document.getElementById("txtNbreJours").value);
 
+
     if(document.getElementById("chkServ").checked == true)
     {
         Toilettage=5;
+        Toilettagemot="avec";
     }
     else
     {
         Toilettage=0;
+        Toilettagemot="sans";
     }
 
-    if(document.getElementById("radChien").checked== true)
+    if(document.getElementById("radChien").checked == true)
     {
         race = "chien";
-        cout = 18,50;
+        cout = 18.50;
     }
     else
     {
         race= "chat";
-        cout=  16,95;
+        cout=  16.95;
     }
+
+    Total=(NbrJours*cout)+(((NbrJours*cout)*14.975)/100)+Toilettage;
+
+    console.log("Vétérinaire responsable est "+ ChoixVet+".Le montant est de "+Total.toFixed(2)+" pour "+NbrJours+" jours ,"+Toilettagemot+" service de toilettage.");
 
 }
