4b81826569b3e1bd74c3fb6fa6a45b786c25c9c3
marcogagne2 <marcogagne2@outlook.fr>
2018-10-24 14:02:08 -0400

    exercise 6 js Terminer

---
+++ Exercises/Exercise 6/JS/traitement.js
@@ -0,0 +1,13 @@
+function btnCalculer_onclick()
+{ 
+     var Un,Cinq,Dix,Ving_Cinq,Total;
+
+    Un=parseInt(document.getElementById("txtUn").value);
+    Cinq= parseInt(document.getElementById("txtCinq").value);
+    Dix=parseInt(document.getElementById("txtDix").value);
+    Ving_Cinq=parseInt(document.getElementById("txtVing_Cinq").value);
+
+    Total=parseFloat((Un*0.01)+(Cinq*0.05)+(Dix*.1)+(Ving_Cinq*.25));
+
+     console.log("Votre argent en Canadien tire est de "+Total+"$");
+}
