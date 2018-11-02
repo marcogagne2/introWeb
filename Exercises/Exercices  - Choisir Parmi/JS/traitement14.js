7457eafe2fee306d38441d76e265e934ccec5df7
marcogagne2 <marcogagne2@outlook.fr>
2018-11-01 23:43:08 -0400

    Exercise 14 terminer

--- Exercises/Exercices  - Choisir Parmi/JS/traitement14.js
+++ Exercises/Exercices  - Choisir Parmi/JS/traitement14.js
@@ -1,5 +1,34 @@
 function btnCalculer_onclick()
 { 
- 	
+ 	var nbr1,nbr2,Symbole,Rep;
+
+ 	nbr1=parseInt(document.getElementById("txtNbre1").value);
+    nbr2=parseInt(document.getElementById("txtNbre2").value);
+    Symbole=document.getElementById("txtOperateur").value;
+
+    if(Symbole=='+')
+    {
+       Rep=nbr1+nbr2;
+    }
+    else
+        {
+            if(Symbole=='-')
+            {
+                Rep=nbr1-nbr2;
+            }
+            else
+            {
+                if(Symbole=='*')
+                {
+                    Rep=nbr1*nbr2;
+                }
+                else
+                    {
+                        Rep=nbr1/nbr2;
+                    }
+            }
+        }
+
+    document.getElementById("lblMessage").innerHTML = "Votre Résultat est: "+ Rep;
 
 }
---
+++ Exercises/Exercise 1/JS/traitement.js
@@ -0,0 +1,9 @@
+function btnCalculer_onclick()
+{ 
+     var PoidsLivres,kg;
+
+     PoidsLivres=parseFloat(document.getElementById("txtPoids").value);
+     kg=(PoidsLivres/2.2);
+     console.log("Votre poids en kg est "+kg);
+
+}
