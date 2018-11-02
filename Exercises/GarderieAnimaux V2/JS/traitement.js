c5fb738b875cdca30b56106e4145804ad66663a0
marcogagne2 <marcogagne2@outlook.fr>
2018-11-02 11:02:09 -0400

    Exercise 14 terminer pour de bon avec switch

---
+++ Exercises/GarderieAnimaux V2/JS/traitement.js
@@ -0,0 +1,63 @@
+function btnCalculer_onclick()
+{ 
+ 	var ChoixVet,NbrJours,NbrHeures,Toilettage,Toilettagemot,race,cout,Total,Rabais,CoutVet;
+
+ 	ChoixVet=document.getElementById("lstVeterinaire").value;
+ 	NbrJours=parseInt(document.getElementById("txtNbreJours").value);
+    NbrHeures=parseInt(document.getElementById("txtNbreHeures").value);
+
+
+    if(document.getElementById("chkServ").checked == true)
+    {
+        Toilettage=5;
+        Toilettagemot="avec";
+    }
+    else
+    {
+        Toilettage=0;
+        Toilettagemot="sans";
+    }
+
+    if(document.getElementById("radChien").checked == true)
+    {
+        race = "chien";
+        cout = 18.50;
+    }
+    else
+    {
+        race= "chat";
+        cout=  16.95;
+    }
+    if(NbrJours>=1&&NbrJours<5)
+    {
+        Rabais=0
+    }
+    else if(NbrJours>=5&&NbrJours<10)
+    {
+        Rabais=(cout*5)/100
+    }
+    else if(NbrJours>=10&&NbrJours<30)
+    {
+        Rabais=(cout*10)/100
+    }
+    else
+    {
+        Rabais=(cout*15)/100
+    }
+    switch(ChoixVet)
+    {
+        case "Audrey Bouchard":CoutVet=NbrHeures*25;
+            break;
+        case "Stéphane Tremblay":CoutVet=NbrHeures*35;
+            break;
+        case "Maxime Simard":CoutVet=NbrHeures*40;
+            break;
+        case "Mélissa Caron":CoutVet=NbrHeures*45;
+            break;
+    }
+
+    Total=((NbrJours*cout)+(((NbrJours*cout)*14.975)/100)+Toilettage+CoutVet)-Rabais;
+
+    console.log("Vétérinaire responsable est "+ ChoixVet+".Le montant est de "+Total.toFixed(2)+" pour "+NbrJours+" jours ,"+Toilettagemot+" service de toilettage.");
+
+}
