b41a73dbe0d13e592676c15c47d4739f2f24ce38
marcogagne2 <marcogagne2@outlook.fr>
2018-11-07 09:56:54 -0500

    function in garderie des animaux

--- Exercises/GarderieAnimaux V2/JS/traitement.js
+++ Exercises/GarderieAnimaux V2/JS/traitement.js
@@ -1,12 +1,35 @@
-function btnCalculer_onclick()
-{ 
- 	var ChoixVet,NbrJours,NbrHeures,Toilettage,Toilettagemot,race,cout,Total,Rabais,CoutVet;
+var ChoixVet,NbrJours,NbrHeures,Toilettage,Toilettagemot,race,cout,Total,Rabais,CoutVet;
 
- 	ChoixVet=document.getElementById("lstVeterinaire").value;
- 	NbrJours=parseInt(document.getElementById("txtNbreJours").value);
+
+function btnCalculer_onclick()
+{
+    ChoixVet=document.getElementById("lstVeterinaire").value;
+    NbrJours=parseInt(document.getElementById("txtNbreJours").value);
     NbrHeures=parseInt(document.getElementById("txtNbreHeures").value);
 
+    fcttoilettage();
+    fctcoutanimaux();
+    fctrabais();
+    choixVette();
 
+    Total=((NbrJours*cout)+(((NbrJours*cout)*14.975)/100)+Toilettage+CoutVet)-Rabais;
+
+    document.getElementById("lblMessage").innerHTML =("Vétérinaire responsable est "+ ChoixVet+".Le montant est de "+Total.toFixed(2)+" pour "+NbrJours+" jours ,"+Toilettagemot+" service de toilettage."+
+    "et le cout de votre vétérinaire est de "+CoutVet)
+}
+function radChoix1_onclick()
+{
+    if(document.getElementById("radimgChien").checked == true)
+    {
+        document.getElementById("Img").innerHTML = "<img src='img/Dog.jpg'>";
+    }
+    else
+    {
+        document.getElementById("Img").innerHTML = "<img src='img/lime-cat.jpg'>";
+    }
+}
+function  fcttoilettage()
+{
     if(document.getElementById("chkServ").checked == true)
     {
         Toilettage=5;
@@ -17,7 +40,9 @@ function btnCalculer_onclick()
         Toilettage=0;
         Toilettagemot="sans";
     }
-
+}
+function fctcoutanimaux()
+{
     if(document.getElementById("radChien").checked == true)
     {
         race = "chien";
@@ -28,6 +53,9 @@ function btnCalculer_onclick()
         race= "chat";
         cout=  16.95;
     }
+}
+function fctrabais()
+{
     if(NbrJours>=1&&NbrJours<5)
     {
         Rabais=0
@@ -44,6 +72,9 @@ function btnCalculer_onclick()
     {
         Rabais=(cout*15)/100
     }
+}
+function choixVette()
+{
     switch(ChoixVet)
     {
         case "Audrey Bouchard":CoutVet=NbrHeures*25;
@@ -55,27 +86,4 @@ function btnCalculer_onclick()
         case "Mélissa Caron":CoutVet=NbrHeures*45;
             break;
     }
-
-    Total=((NbrJours*cout)+(((NbrJours*cout)*14.975)/100)+Toilettage+CoutVet)-Rabais;
-
-    console.log("Vétérinaire responsable est "+ ChoixVet+".Le montant est de "+Total.toFixed(2)+" pour "+NbrJours+" jours ,"+Toilettagemot+" service de toilettage.");
-    document.getElementById("lblMessage").innerHTML =("Vétérinaire responsable est "+ ChoixVet+".Le montant est de "+Total.toFixed(2)+" pour "+NbrJours+" jours ,"+Toilettagemot+" service de toilettage.")
-}
-function radChoix1_onclick()
-{
-    var chien,chat;
-
-    chien=document.getElementById("radimgChien").value;
-    chat=document.getElementById("radimgChat").value;
-
-    if(document.getElementById("radimgChien").checked == true)
-    {
-        document.getElementById("Img").innerHTML = "<img src='img/Dog.jpg'>";
-    }
-    if(document.getElementById("radimgChat").checked == true)
-    {
-        document.getElementById("Img").innerHTML = "<img src='img/lime-cat.jpg'>";
-    }
-
-
 }
