76b86b1751d2f67b043c58ab658eea9f7a3cc8d8
marcogagne2 <marcogagne2@outlook.fr>
2018-11-07 10:59:21 -0500

    debut exercise joueur

---
+++ Exercises/Exercice _ Joueurs/JS/traitement.js
@@ -0,0 +1,27 @@
+var tabJoueur,tabPoint,i,Note,Nom,cpt;
+
+tabjoueur=new Array(4);
+tabPoint=new Array(4);
+
+Nom=document.getElementById("txtNom").value;
+Note=parseInt(document.getElementById("txtPoints").value);
+
+function btnRechercher_onclick()
+{
+    for(i=0;i<tabjoueur.lenght,i++;)
+    {
+        if(i<5)
+        {
+
+        }
+        else
+            {
+
+            }
+
+    }
+}
+
+
+
+
