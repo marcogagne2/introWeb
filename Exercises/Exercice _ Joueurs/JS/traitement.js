9f90dd7d0d6021d4e53e76967f7e4a85e839cd92
marcogagne2 <marcogagne2@outlook.fr>
2018-11-09 11:20:37 -0500

    debut exercise joueur

--- Exercises/Exercice _ Joueurs/JS/traitement.js
+++ Exercises/Exercice _ Joueurs/JS/traitement.js
@@ -1,27 +1,81 @@
-var tabJoueur,tabPoint,i,Note,Nom,cpt;
+var tabjoueur,tabPoint,Note,Nom,cpt;
 
 tabjoueur=new Array(4);
 tabPoint=new Array(4);
+cpt=0;
 
-Nom=document.getElementById("txtNom").value;
-Note=parseInt(document.getElementById("txtPoints").value);
-
-function btnRechercher_onclick()
+function btnAjouter_onclick()
 {
-    for(i=0;i<tabjoueur.lenght,i++;)
+    Nom=document.getElementById("txtNom").value;
+    Note=parseInt(document.getElementById("txtPoints").value);
+    tabjoueur[cpt]=Nom;
+    tabPoint[cpt]=Note;
+    btndisable_desactiver();
+
+    switch(cpt)
     {
-        if(i<5)
-        {
-
-        }
-        else
-            {
-
-            }
-
+        case 0: document.getElementById("lblNbreJoueur").innerHTML =("Numéro du joueur 2");
+        break;
+        case 1:document.getElementById("lblNbreJoueur").innerHTML =("Numéro du joueur 3");
+        break;
+        case 2:document.getElementById("lblNbreJoueur").innerHTML =("Numéro du joueur 4")
+        ;break;
+    }
+    cpt++;
+}
+function btndisable_desactiver()
+{
+    if(cpt==3)
+    {
+        document.getElementById("btnAjouter").disabled=true;
+        document.getElementById("btnRechercher").disabled=false;
+        document.getElementById("btnTrouverMoy").disabled=false;
+        document.getElementById("btnTrouverMeilleur").disabled=false;
+        document.getElementById("btnTrouverPire").disabled=false;
     }
 }
+function btnTrouverMoy_onclick()
+{
+    var Moy,TotNbr;
 
+    TotNbr=0;
 
+    for(i=0;i<tabjoueur.length;i++)
+    {
+        TotNbr += tabPoint[i];
+    }
 
+    Moy=TotNbr/tabPoint.length;
+    document.getElementById("lblReponse").innerHTML =("Votre Moyenne est de "+Moy);
+}
+function btnTrouverMeilleur_onclick()
+{
+    var vargrande,personne;
+    vargrande = 0;
 
+    for (i = 0; i < tabjoueur.length; i++)
+    {
+        if (tabPoint[i] > vargrande)
+        {
+            vargrande = tabPoint[i];
+            personne = tabjoueur[i];
+        }
+    }
+    document.getElementById("lblReponse").innerHTML =("Votre nombre le plus grand est "+vargrande+" et il appartient à "+personne);
+}
+function btnTrouverPire_onclick()
+{
+    var varpettite,personne;
+
+    varpettite=999999999999;
+
+    for (i = 0; i < tabjoueur.length; i++)
+    {
+        if (tabPoint[i] < varpettite)
+        {
+            varpettite = tabPoint[i];
+            personne = tabjoueur[i];
+        }
+    }
+    document.getElementById("lblReponse").innerHTML =("Votre nombre le plus petit est "+varpettite+" et il appartient à "+personne);
+}
